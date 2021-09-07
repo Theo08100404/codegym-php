af8c2157b2c8be021f453214c097ff403bcb638b
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-09-07 18:57:26 +0900

    更新2回目

--- html/knock/bubble_sort/index.php
+++ html/knock/bubble_sort/index.php
@@ -3,7 +3,15 @@ $nums = [100, 5, 2, -10, 8, 10, -1, 15, 1, -100];
 
 $idxLast = count($nums) - 1;
 
-//ここにコードを書きましょう。
+for ($i = 0; $i < $idxLast; $i++) {
+    for ($j = $idxLast; $i < $j; $j--) {
+        if ($nums[$j] < $nums[$j - 1]) {
+            $temp = $nums[$j - 1];
+            $nums[$j - 1] = $nums[$j];
+            $nums[$j] = $temp;
+        }
+    }
+}
 
 echo '<pre>';
 print_r($nums);
--- html/study/submit.php
+++ html/study/submit.php
@@ -10,13 +10,37 @@
 
 <body>
     <header>
-        <h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+        <h1 class="font-weight-normal">フォームの受信</h1>
     </header>
     <main>
         <h2>Practice</h2>
         <pre>
-            お名前<?php print($_REQUEST['comment']);
+            お名前：<?php print(htmlspecialchars($_REQUEST['comment'], ENT_QUOTES));
+
+
+
+
                 ?>
+            性別：<?php print(htmlspecialchars($_POST['gender'], ENT_QUOTES));
+                ?>
+            
+            ご予約日:
+            <?php
+            foreach ($_POST['reserve'] as $reserve) {
+                print(htmlspecialchars($reserve, ENT_QUOTES) . ' ');
+            }
+            ?>
+            <?php
+            $zip = '987-6543';
+            $zip = mb_convert_kana($zip, 'a', 'utf-8');
+            if (preg_match("/\A\d{3}[-]\d{4}\z/", $zip)) {
+                print('郵便番号: 〒' . $zip);
+            } else {
+                print('※郵便番号を　　123-4567の形式でご記入下さい');
+            }
+            ?>
+            変数の値：<?php print($value); ?>
+            Cookieの値: <?php print($_COOKIE['sanve_message']); ?>
 
 
 
