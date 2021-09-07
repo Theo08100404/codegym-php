a28bb13813a522d17da8fcde38d7670fe08e799f
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-09-07 15:19:04 +0900

    リクエストパラメータ不良

---
+++ html/study/submit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width , initial-arcle=1 ,shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <title>よくわかるPHPの教科書</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+    </header>
+    <main>
+        <h2>Practice</h2>
+        <pre>
+            お名前<?php print($_REQUEST['comment']);
+                ?>
+
+
+
+        </pre>
+    </main>
+</body>
+
+</html>
