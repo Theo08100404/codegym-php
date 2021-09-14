e2dc7f11596583d503dddb05b2199bf0d5d948f3
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-09-14 17:43:46 +0900

    chaptar6完了

---
+++ html/study/post/join/thanks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width , initial-arcle=1 ,shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <title>mini_bbs</title>
+</head>
+
+<p>ユーザー登録が完了しました</p>
+<p><a href="../">ログインする</a></p>
+
+</html>
